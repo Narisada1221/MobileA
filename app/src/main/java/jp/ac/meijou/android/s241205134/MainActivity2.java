1dac349bb18883d84c844fe509ba9b47cb1f70a1
Narisada1221 <241205134@ccmailg.meijo-u.ac.jp>
2025-09-03 15:17:20 +0900

    遷移

--- app/src/main/java/jp/ac/meijou/android/s241205134/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205134/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.s241205134;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205134.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205134.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.button2.setOnClickListener( v ->{
+           var intent = new Intent(this, MainActivity3.class);
+           startActivity(intent);
+        });
+
+        binding.button3.setOnClickListener(v ->{
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        });
+
+        binding.button7.setOnClickListener(v->  {
+            var text = binding.editTextText.getText().toString();
+            var intent = new Intent(this,MainActivity4.class);
+            intent.putExtra("title",text);
+            startActivity(intent);
         });
     }
 }
