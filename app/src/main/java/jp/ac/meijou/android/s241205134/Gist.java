767dc55e317538d1dcfa8314704dbfdb86c77dd2
Narisada1221 <241205134@ccmailg.meijo-u.ac.jp>
2025-09-04 12:19:45 +0900

    Webサーバからテキストを取得し、TextViewに反映

---
+++ app/src/main/java/jp/ac/meijou/android/s241205134/Gist.java
@@ -0,0 +1,15 @@
+package jp.ac.meijou.android.s241205134;
+
+import java.util.Map;
+public class Gist {
+    public Map<String, GistFile> files;
+
+
+
+
+
+    public static class GistFile {
+        public String content;
+    }
+
+}
--- app/src/main/java/jp/ac/meijou/android/s241205134/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205134/MainActivity.java
@@ -12,7 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
 import jp.ac.meijou.android.s241205134.databinding.ActivityMainBinding;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
